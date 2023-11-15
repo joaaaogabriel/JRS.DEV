eb37a8102f539cf88cbc83372a9540d08f881d52
JRoberto.dev <128549329+Joao-Roberto7@users.noreply.github.com>
2023-11-15 13:52:08 -0300

    Update Jogos.js

--- Jogos.js
+++ Jogos.js
@@ -1,25 +1,4 @@
 const games = [
-  { title: "Super Mario Bros", year: 1985 },
-  { title: "Pac-Man", year: 1980 },
-  { title: "Tetris", year: 1984 },
-  { title: "Donkey Kong", year: 1981 },
-  { title: "Street Fighter II", year: 1991 },
-  { title: "Space Invaders", year: 1978 },
-  { title: "Sim City", year: 1989 },
-  { title: "The Legend of Zelda", year: 1986},
-  { title: "Doom", year: 1993 },
-  { title: "Super Mario 64", year: 1996 },
-  { title: "Pokémon", year: 1996 },
-  { title: "Resident Evil", year: 1996 },
-  { title: "Final Fantasy VII", year: 1997 },
-  { title: "Half-Life", year: 1998 },
-  { title: "Metal Gear Solid", year: 1998 },
-  { title: "Grand Theft Auto III", year: 2001 },
-  { title: "Guitar Hero", year: 2005 },
-  { title: "Angry Birds", year: 2009 },
-  { title: "Farmville", year: 2009 },
-  { title: "League of Legends", year: 2009 },
-  { title: "Minecraft", year: 2011 },
   { title: "Super Mario Bros", year: 1985, showMoreDetails: false, details: 'lorem ipsun' },
   { title: "Pac-Man", year: 1980, showMoreDetails: false, details: 'lorem ipsun' },
   { title: "Tetris", year: 1984, showMoreDetails: false, details: 'lorem ipsun' },
@@ -42,7 +21,7 @@ const games = [
   { title: "League of Legends", year: 2009, showMoreDetails: false, details: 'lorem ipsun' },
   { title: "Minecraft", year: 2011, showMoreDetails: false, details: 'lorem ipsun' },
 
-  // Adicione mais jogos conforme necessário
+  
 ];
 
 const gameList = document.querySelector('.game-list');
